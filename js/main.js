71de6525421b8894cdb37dc6cb189e43ec42edb6
Liudmila Skripkina <msk.revizor@gmail.com>
2023-02-13 16:02:36 +0300

    Add adaptive for the header-section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/*  меню бургер  */
+// меню бургер
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.head__nav');
 
@@ -28,7 +28,7 @@ if (anchors.length > 0) {
       if (anchor.dataset.goto && document.querySelector(anchor.dataset.goto)) {
          const gotoBlock = document.querySelector(anchor.dataset.goto);
          // учитываем высоту шапки
-         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('.head').offsetHeight + 50;
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('.head').offsetHeight + 120;
 
          if (iconMenu.classList.contains('menu-open')) {
             document.body.classList.remove('lock');
@@ -43,4 +43,42 @@ if (anchors.length > 0) {
          e.preventDefault();
       }
    }
+}
+
+// скрываем скролл, добавляя паддинг body и объектам с position: fixed
+function bodyLock() {
+   const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+   if (lockPadding.length > 0) {
+      for (let i = 0; i < lockPadding.length; i++) {
+         const el = lockPadding[i];
+         el.getElementsByClassName.paddingRight = lockPaddingValue;
+      }
+   }
+   body.style.paddingRight = lockPaddingValue;
+   body.classList.add('lock');
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+// открываем скролл и убираем паддинг у body и объектов с position: fixed
+function bodyUnlock() {
+   setTimeout(function () {
+      if (lockPadding.length > 0) {
+         for (let i = 0; i < lockPadding.length; i++) {
+            const el = lockPadding[i];
+            el.style.paddingRight = '0px';
+         }
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+   }, timeout);
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
 }
